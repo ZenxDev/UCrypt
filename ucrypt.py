9a35c75390ac3ab6e311431b5ea7c1d64514e420
ZenxDev <93379662+ZenxDev@users.noreply.github.com>
2022-01-04 04:14:16 +0800

    update ucrypt.py

--- ucrypt.py
+++ ucrypt.py
@@ -1,1 +1,31 @@
-eJxdUrtu3DAQNNzpvmLDigIEVoaLA+ImcHFdgMC1waOWFgGKJLgU7pQg/id/Uv4kSz0OhtWQmuHMDnf57+P+7q7k+Xho3JhiLpBm6948lg5GnWnQvoPf3p07iNTBWRM+PhyaQ2O8JoIXk+dUZMvyRgjRR0Mlu/AGNmZ4DgvLeBU0PVrWh4BZEnq7aJpIimYqOEphPOos2oriAN9vQdS6vLLlqIsULz8W1w4EeR3KqshYphwAh1spXKsvtTooeC1rRcvWMWGQFWKTrWSvi2bGqoy6lwtklfGRcP0ZKrt1RPXTmEhWyco55mqTlIljykgkh41BZtamqfPjA2eKPcrBtarHZSs0GefWCPbyJdplx9Ulu8I6/HxXK35NxnAxO3k/7/cFF0qEP9Xh77el8ZNh231QhybxfIqcjNpm0TJWroXPuJAm7u+pLmCdR3h/ghrhk2jvKitY+PXDq0GOcFpe0nPOMR93rfi5jRMcQYg1JxXtPfYKTusWXIGJ+PUcIbm0H7i9A3EL3/4HfCLSrg==
+import marshal, base64, zlib, sys
+
+stri = input("File ~> ")
+pswd = input("Password ~> ")
+arr = []
+key = len(pswd)
+
+f = open(stri, "r")
+fr = f.read()
+f.close()
+
+def encrypt(text):
+	for char in text:
+		arr.append(chr(ord(char)+key))
+	res = "".join(arr)
+	return res
+
+enc_bytes = marshal.dumps(encrypt(fr))
+lib = zlib.compress(enc_bytes)
+result = base64.b64encode(lib).decode("ascii")
+print("Result : "+result)
+
+izin = input("Want to save this?[y/n]:")
+if izin == "y" or izin == "Y":
+	fw = open("encrypted.txt", "w")
+	fw.write("Encrypted By ZenxDev\n\n"+result)
+	print("Save to dummy.txt")
+elif izin == "n" or izin == "N":
+	sys.exit()
+else:
+	sys.exit()
